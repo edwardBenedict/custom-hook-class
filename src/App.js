da753b5abe9ec65b40c76575001893070ab83cd8
edwardBenedict <edwardbenedict92@gmail.com>
2022-04-09 16:41:51 +0200

    v2

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import "./App.css";
 import useFetch from "./hooks/useFetch";
 
 function App() {
-  fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((response) => {
-      console.log('res',response);
-      return response.json();
-    })
-    .then((data) => console.log(data));
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
 
   return (
     <div className="App">
       <h1>Custom Hook</h1>
+      {data?.map((item) => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+        </div>
+      ))}
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
     </div>
   );
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,18 +1,27 @@
-import {useState, useEffect} from 'react'
-function useFetch() {
-    const [data, setData] = useState()
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(false)
+import { useState, useEffect } from "react";
+function useFetch(url, initialdata = []) {
+  const [data, setData] = useState(initialdata);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => {
-        
-        return response.json()})
-    .then(data => setData(data));
-}, [])
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          setError("Something went wrong!");
+          setLoading(false);
+        }else{
+            return response.json();
+        }
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, [url]);
 
-    
+  return { data, loading, error };
 }
 
-export default useFetch
+export default useFetch;
